4f787bb5172284a3bc2ded8acbc5443c09882b1c
darciogleidson <darciogleidson@gmail.com>
2022-12-20 21:20:26 -0300

    lendo excel

--- app/app.py
+++ app/app.py
@@ -1,18 +1,8 @@
-#import xlrd
-#
-#arq = xlrd.open_workbook('dados.xlsx')
-#plan = arq.sheet_by_name("dados1")
-#
-#x = plan.col_values(0)
-#y = plan.col_values(1)
+import pandas as pd
+excel = pd.read_excel("app\dadoss.xlsx")
 
-import os,xlrd
-filename = os.path.join(subdir, 'dados2.xls')
-book = xlrd.open_workbook(filename)
-print("The number of worksheets is {0}".format(book.nsheets))
-print("Worksheet name(s): {0}".format(book.sheet_names()))
-sh = book.sheet_by_index(0)
-print("{0} {1} {2}".format(sh.name, sh.nrows, sh.ncols))
-print("Cell D30 is {0}".format(sh.cell_value(rowx=29, colx=3)))
-for rx in range(sh.nrows):
-    print(sh.row(rx))
+mes = input("Qual o mes ?")
+
+analise = excel.sort_values(by=f'{mes}', ascending=False)
+
+print(analise)
